88eead9900128c85427ed4c05fd435b7d8477a34
suhel45 <suhel.ict45@gmail.com>
2025-01-21 13:19:11 +0600

    footer component added

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,7 +1,9 @@
+import '@fortawesome/fontawesome-free/css/all.min.css';
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import Footer from './shared/Footer/Footer';
 
 function App() {
   const [count, setCount] = useState(0)
@@ -28,6 +30,7 @@ function App() {
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
+      <Footer></Footer>
     </>
   )
 }
