0a2a296be598232455a668a21db8dea77fb3b4c8
Mahmud803 <mahmudimran803@gmail.com>
2025-01-23 23:29:53 +0600

    learn number and math in js

---
+++ Javascript/math_number.js
@@ -0,0 +1,41 @@
+const score=400
+// console.log(score);
+
+const value=new Number(100)
+// console.log(value);
+
+// console.log(value.toString().length);
+// console.log(value.toFixed(2));
+
+const otherNumber=123.8996
+
+// console.log(otherNumber.toPrecision(4));
+
+const hundred=1000000
+// console.log(hundred.toLocaleString('en-IN'));
+
+//++++++++++++++++++math++++++++++++++++++++++++++++
+
+console.log(Math);
+console.log(Math.abs(-43));
+console.log(Math.round(4.4));
+console.log(Math.ceil(4.2));
+console.log(Math.floor(4.6));
+console.log(Math.min(5,2,6,3,4,6));
+console.log(Math.max(43,23,1,2,5,6,1));
+console.log(Math.random());
+console.log(Math.random()*10);
+
+const min=10
+const max=20
+
+console.log(Math.floor(Math.random() * (max-min +1)) + min);
+
+console.log(Number.isNaN(NaN));
+
+console.log(Number.MIN_VALUE);
+
+console.log(Number.MAX_VALUE)
+
+
+
