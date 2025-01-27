e847b7c810c40c5d77303f982cc320d0396e07fd
shuvojr13 <shuvojr1379@gmail.com>
2025-01-27 13:54:21 +0600

    Add Hero Section of Home Page

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,12 +1,14 @@
 import '@fortawesome/fontawesome-free/css/all.min.css';
 import Footer from "./shared/Footer/Footer"
 import Nav from "./components/Nav"
+import Hero from './components/Hero';
 
 
 function App() {
   return (
     <>
       <Nav/>
+      <Hero/>
       <h1 className="font-font text-3xl text-red-400">Hello World</h1>
       <Footer></Footer>
     </>
---
+++ client/src/components/Hero.tsx
@@ -0,0 +1,11 @@
+import HeroImage from '../assets/hero.svg';
+
+const Hero = () => {
+  return (
+    <div className="hero-section text-center">
+      <img src={HeroImage} alt="Hero" className="w-full h-auto mx-auto" />
+    </div>
+  );
+};
+
+export default Hero;
