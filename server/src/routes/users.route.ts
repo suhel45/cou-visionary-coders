99678446a078f6665451f6ec69769e2412204226
suhel45 <suhel.ict45@gmail.com>
2025-02-01 16:56:23 +0600

    import createuser to signup route

--- server/src/routes/users.route.ts
+++ server/src/routes/users.route.ts
@@ -1,4 +1,5 @@
 import express from 'express';
+import { userController } from '../controllers/user.controller';
 const router = express.Router();
 
-router.post('/signup',)
+router.post('/signup',userController.createUser);
