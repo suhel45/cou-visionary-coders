9434a24dd42227f8c5730665e4eb88429c59b4b1
suhel45 <suhel.ict45@gmail.com>
2025-02-01 21:14:21 +0600

    update schema to the requirement

--- server/src/models/user.Model.ts
+++ server/src/models/user.Model.ts
@@ -1,10 +1,16 @@
 import { model, Schema } from "mongoose";
 import {IUser} from '../interfaces/users.interface'
 import connectDB from "../db/database.connection";
+
 connectDB();
 const userSchema = new Schema<IUser>({
-    name: { type: String, required: true, trim: true },
     email: { type: String, required: true, unique: true, lowercase: true },
+    birthdate: { type: Date, required: true },
+    phoneNumber: { type: String, required: true, unique: true },
+    weight: { type: Number, required: true },
+    height: { type: Number, required: true },
+    complexion: { type: String, required: true},
+    bloodGroup: { type: String, required: true, enum: ["A+", "A-", "B+", "B-", "O+", "O-", "AB+", "AB-"] },
     password: { type: String, required: true, minlength: 6 },
 });
 const userModel = model<IUser>('User',userSchema);
