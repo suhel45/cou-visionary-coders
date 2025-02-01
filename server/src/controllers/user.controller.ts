0a12752ccd6ccac725a795ec8431a3c01c32660e
suhel45 <suhel.ict45@gmail.com>
2025-02-01 16:44:45 +0600

    password encrypted received from user req body

--- server/src/controllers/user.controller.ts
+++ server/src/controllers/user.controller.ts
@@ -1,11 +1,12 @@
 import { Request, Response } from "express";
 import bcrypt from 'bcrypt';
+import userModel from "../models/user.Model";
 const saltRounds = 10;
 const createUser = async(req:Request,res:Response)=>{
-    const {email,password} = req.body;
+    const {name,email,password} = req.body;
     try {
         const hasedpassword = await bcrypt.hash(password,saltRounds);
-        
+        const users = new userModel({name,email,password:hasedpassword});
     } catch (error) {
         
     }
