7698ad3d8d67e1e5a496f310b2a48d75f2d3c0ec
suhel45 <suhel.ict45@gmail.com>
2025-02-01 23:36:35 +0600

    secure http request headers

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,10 +1,11 @@
+import helmet from "helmet";
 import express, { Express, Request, Response } from 'express';
 import cors from 'cors';
 import { userRoute } from './routes/users.route';
 const app: Express = express();
-app.disable('x-powered-by');
 app.use(cors());
 app.use(express.json());
+app.use(helmet());
 
 app.use('/api/v1', userRoute);
 
