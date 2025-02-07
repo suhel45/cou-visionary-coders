c6328b75e13c6ed8fbcdcf1f951cc2ddc4764da4
shuvojr13 <shuvojr1379@gmail.com>
2025-02-07 15:39:44 +0600

    add routing

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,18 +1,35 @@
-import '@fortawesome/fontawesome-free/css/all.min.css';
-import Footer from "./shared/Footer/Footer"
-import Nav from "./components/Nav"
-import Hero from './components/Hero';
+import { Route, Routes } from "react-router-dom";
+import Nav from "./components/Nav";
+import SignUp from "./pages/signup/pages";
 
-
-function App() {
-  return (
-    <>
-      <Nav/>
-      <Hero/>
-      <h1 className="font-font text-3xl text-red-400">Hello World</h1>
-      <Footer></Footer>
-    </>
-  )
+function NoMatch() {
+  return <Error />;
 }
 
-export default App
+export default function App() {
+  return (
+    <div className="flex flex-col min-h-screen">
+      {/* Nav component fixed at the top */}
+     <Nav/>
+
+      {/* Content area */}
+      <div className="flex-1 overflow-y-auto">
+       
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/home" element={<Home />} />
+            <Route path="/signup" element={<SignUp />} />
+            <Route path="/login" element={<Login />} />
+            
+
+           
+            <Route path="*" element={<NoMatch />} />
+          </Routes>
+       
+      </div>
+
+      {/* Footer component fixed at the bottom */}
+      <Footer />
+    </div>
+  );
+}
