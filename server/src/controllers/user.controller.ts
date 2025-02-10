ff866ebac9a1ab01044bd6124d456b3abe725de1
suhel45 <suhel.ict45@gmail.com>
2025-02-10 13:05:20 +0600

    error hadle to try uncredential information to register

--- server/src/controllers/user.controller.ts
+++ server/src/controllers/user.controller.ts
@@ -3,13 +3,21 @@ import { userService } from '../services/users.service';
 import { IUser } from '../interfaces/users.interface';
 const createUser = async (req: Request, res: Response) => {
   try {
-    const user:IUser = req.body;
+    const userInfo:IUser = req.body;
     //will call service function to send this data
-    const result = await userService.createUserIntoDB(user);
+    const result = await userService.createUserIntoDB(userInfo);
     //send response
-    res.status(201).send({ message: 'User registered successfully' });
+    res.status(200).json({ 
+      success:true,
+      message: 'User registered successfully',
+      data:result 
+    });
   } catch (error) {
-    res.status(500).send({ errors: 'registration failed', error });
+    if (error instanceof Error && error.message === 'User already exists!') {
+      res.status(400).json({ message: error.message });
+    } else {
+      res.status(500).send({ errors: 'registration failed', error });
+    }
   }
 };
 export const userController = {
