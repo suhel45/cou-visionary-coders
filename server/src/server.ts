b925ed8a81a39db1281c77cc5d0c6f03e5fdba8c
suhel45 <suhel.ict45@gmail.com>
2025-02-10 19:01:43 +0600

    add server initialization requires enhancement of signal termination

--- server/src/server.ts
+++ server/src/server.ts
@@ -3,6 +3,14 @@ import app from './app';
 dotenv.config();
 const port = process.env.PORT || 3000;
 
-app.listen(port, () => {
+const server = app.listen(port, () => {
   console.log(`[server]: Server is running at http://localhost:${port}`);
+});
+
+process.on('SIGTERM', () => {
+  console.log('SIGTERM signal received: closing HTTP server');
+  server.close(() => {
+    console.log('HTTP server closed');
+    process.exit(0);
+  });
 });
