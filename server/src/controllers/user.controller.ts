7f2fc8c11d1d38f9e5d35672a6e84e5bca3f6037
suhel45 <suhel.ict45@gmail.com>
2025-02-10 12:45:42 +0600

    userservice componet create to handle business logic

--- server/src/controllers/user.controller.ts
+++ server/src/controllers/user.controller.ts
@@ -1,43 +1,12 @@
 import { Request, Response } from 'express';
-import { escape } from 'validator';
-import bcrypt from 'bcrypt';
-import userModel from '../models/user.Model';
-const saltRounds = 10;
+import { userService } from '../services/users.service';
+import { IUser } from '../interfaces/users.interface';
 const createUser = async (req: Request, res: Response) => {
   try {
-    const {
-      email,
-      birthdate,
-      phoneNumber,
-      weight,
-      height,
-      complexion,
-      bloodGroup,
-      password,
-      confirmPassword,
-    } = req.body;
-    // Ensure passwords match
-    if (password !== confirmPassword) {
-      res.status(400).json({ message: 'Passwords do not match!' });
-    }
-    const sanitizedEmail = escape(email);
-    // Check if user already exists
-    const existingUser = await userModel.findOne({ email: sanitizedEmail });
-    if (existingUser) {
-      res.status(400).json({ message: 'User already exists!' });
-    }
-    const hasedpassword = await bcrypt.hash(password, saltRounds);
-    const user = new userModel({
-      email,
-      password: hasedpassword,
-      birthdate,
-      mobile:phoneNumber,
-      weight,
-      height,
-      complexion,
-      bloodGroup,
-    });
-    user.save();
+    const user:IUser = req.body;
+    //will call service function to send this data
+    const result = await userService.createUserIntoDB(user);
+    //send response
     res.status(201).send({ message: 'User registered successfully' });
   } catch (error) {
     res.status(500).send({ errors: 'registration failed', error });
