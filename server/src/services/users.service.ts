11d8bb2ae4d4adad5fd2c14bd8e45c336120386b
suhel45 <suhel.ict45@gmail.com>
2025-02-11 16:38:51 +0600

    generate toke for login user

--- server/src/services/users.service.ts
+++ server/src/services/users.service.ts
@@ -2,6 +2,10 @@ import { ILoginInfo, IUser } from '../interfaces/users.interface';
 import userModel from '../models/user.Model';
 import { escape } from 'validator';
 import argon2 from 'argon2';
+import jwt from 'jsonwebtoken';
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 const createUserIntoDB = async (user: IUser) => {
   const { username, contact, email, password } = user;
@@ -35,6 +39,8 @@ const loginUserFromDB = async (loginInfo: ILoginInfo): Promise<void> => {
   if(!isPasswordValid) {
     throw new Error('Invalid password');
   }
+  const token = jwt.sign({id:user._id,email:email},process.env.JWT_SECRET_KEY);
+  console.log(token);
 };
 
 export const userService = {
