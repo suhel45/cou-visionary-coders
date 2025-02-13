03277bb7f33ea2ecc176f2fd31af2ebae2aee58e
suhel45 <suhel.ict45@gmail.com>
2025-02-13 13:23:58 +0600

    error message throw if not found secret key

--- server/src/middleware/authMiddleware.ts
+++ server/src/middleware/authMiddleware.ts
@@ -1,6 +1,16 @@
 import { Request, Response, NextFunction } from "express";
+import jwt from 'jsonwebtoken';
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 const verifyToken = (req:Request, res:Response, next:NextFunction) => {
     const token = req.cookies.token;
+
     if (!token) return res.status(401).json({ message: "Access denied" });
+
+    const secretKey = process.env.JWT_SECRET_KEY;
+    if (!secretKey) return res.status(500).json({ message: "Internal server error" });
+    
+    const decoded = jwt.verify(token, secretKey);
 }
