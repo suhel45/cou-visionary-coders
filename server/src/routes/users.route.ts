48e67c2debc64569d4091ca07d06ed619aa3b0c1
suhel45 <suhel.ict45@gmail.com>
2025-02-14 00:17:04 +0600

    remove authmiddleware test route

--- server/src/routes/users.route.ts
+++ server/src/routes/users.route.ts
@@ -1,12 +1,9 @@
-import express, { Request, Response } from 'express';
+import express from 'express';
 import { userController } from '../controllers/user.controller';
-import { verifyToken } from '../middleware/authMiddleware';
+
 const router = express.Router();
 
 router.post('/signup', userController.createUser);
 router.post('/login', userController.loginUser);
-router.get('/user', verifyToken, async (req: Request, res: Response) => {
-  res.json({ name: 'suhel', id: 5 });
-});
 
 export const userRoute = router;
