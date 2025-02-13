9041cea8c52acdd0b56e6118cdfe34448ebb0310
suhel45 <suhel.ict45@gmail.com>
2025-02-13 13:35:43 +0600

    verify token with secret key and decoded

--- server/src/middleware/authMiddleware.ts
+++ server/src/middleware/authMiddleware.ts
@@ -1,16 +1,22 @@
 import { Request, Response, NextFunction } from "express";
 import jwt from 'jsonwebtoken';
 import dotenv from 'dotenv';
+import { resolve } from "path";
 
 dotenv.config();
 
-const verifyToken = (req:Request, res:Response, next:NextFunction) => {
+const verifyToken = async(req:Request, res:Response, next:NextFunction) => {
     const token = req.cookies.token;
 
     if (!token) return res.status(401).json({ message: "Access denied" });
 
     const secretKey = process.env.JWT_SECRET_KEY;
     if (!secretKey) return res.status(500).json({ message: "Internal server error" });
-    
-    const decoded = jwt.verify(token, secretKey);
+
+    const decoded = await new Promise((resolve, reject) => {
+        jwt.verify(token,secretKey, (err, decoded) => {
+            if (err) return reject(err);
+            resolve(decoded);
+        })
+    })
 }
