d7c9d0172d88d3232bf41d835223f42faa0ddacb
suhel45 <suhel.ict45@gmail.com>
2025-02-15 20:51:25 +0600

    name attribute added of each input field to get data of submission

--- client/src/components/PersonalInformation.tsx
+++ client/src/components/PersonalInformation.tsx
@@ -22,139 +22,150 @@ const PersonalInformation = () => {
       />
       <CardContent>
         <form>
-        <Grid container spacing={3}>
-          {/* Biodata No */}
-          <Grid item xs={12} md={6} >
-            <TextField
-              fullWidth
-              label="Biodata No"
-              id="biodataNo"
-              placeholder="Enter Biodata Number"
-            />
-          </Grid>
+          <Grid container spacing={3}>
+            {/* Biodata No */}
+            <Grid item xs={12} md={6}>
+              <TextField
+                fullWidth
+                label="Biodata No"
+                id="biodataNo"
+                name="biodataNo"
+                placeholder="Enter Biodata Number"
+              />
+            </Grid>
 
-          {/* Biodata Type */}
-          <Grid item xs={12} md={6}>
-            <TextField
-              fullWidth
-              select
-              label="Biodata Type"
-              id="biodataType"
-              defaultValue="">
-              <MenuItem value="bride">Bride</MenuItem>
-              <MenuItem value="groom">Groom</MenuItem>
-            </TextField>
-          </Grid>
+            {/* Biodata Type */}
+            <Grid item xs={12} md={6}>
+              <TextField
+                fullWidth
+                select
+                label="Biodata Type"
+                name="biodataType"
+                id="biodataType"
+                defaultValue="">
+                <MenuItem value="bride">Bride</MenuItem>
+                <MenuItem value="groom">Groom</MenuItem>
+              </TextField>
+            </Grid>
 
-          {/* Marital Status */}
-          <Grid item xs={12} md={6} >
-            <TextField
-              fullWidth
-              select
-              label="Marital Status"
-              id="maritalStatus"
-              defaultValue="">
-              <MenuItem value="single">Single</MenuItem>
-              <MenuItem value="divorced">Divorced</MenuItem>
-            </TextField>
-          </Grid>
+            {/* Marital Status */}
+            <Grid item xs={12} md={6}>
+              <TextField
+                fullWidth
+                select
+                label="Marital Status"
+                id="maritalStatus"
+                name="maritalStatus"
+                defaultValue="">
+                <MenuItem value="single">Single</MenuItem>
+                <MenuItem value="divorced">Divorced</MenuItem>
+              </TextField>
+            </Grid>
 
-          {/* Date of Birth */}
-          <Grid item xs={12} md={6} >
-            <TextField
-              fullWidth
-              label="Date of Birth"
-              id="dob"
-              type="date"
-              InputLabelProps={{ shrink: true }}
-            />
-          </Grid>
+            {/* Date of Birth */}
+            <Grid item xs={12} md={6}>
+              <TextField
+                fullWidth
+                label="Date of Birth"
+                id="dob"
+                name=""
+                type="date"
+                InputLabelProps={{ shrink: true }}
+              />
+            </Grid>
 
-          {/* Height */}
-          <Grid item xs={12} md={6} >
-            <TextField
-              fullWidth
-              label="Height (cm)"
-              id="height"
-              type="number"
-              placeholder="Enter height"
-            />
-          </Grid>
+            {/* Height */}
+            <Grid item xs={12} md={6}>
+              <TextField
+                fullWidth
+                label="Height (cm)"
+                id="height"
+                name="height"
+                type="number"
+                placeholder="Enter height"
+              />
+            </Grid>
 
-          {/* Weight */}
-          <Grid item xs={12} md={6} >
-            <TextField
-              fullWidth
-              label="Weight (kg)"
-              id="weight"
-              type="number"
-              placeholder="Enter weight"
-            />
-          </Grid>
+            {/* Weight */}
+            <Grid item xs={12} md={6}>
+              <TextField
+                fullWidth
+                label="Weight (kg)"
+                id="weight"
+                name="weight"
+                type="number"
+                placeholder="Enter weight"
+              />
+            </Grid>
 
-          {/* Complexion */}
-          <Grid item xs={12} md={6} >
-            <TextField
-              fullWidth
-              select
-              label="Complexion"
-              id="complexion"
-              defaultValue="">
-              <MenuItem value="fair">Fair</MenuItem>
-              <MenuItem value="medium">Medium</MenuItem>
-              <MenuItem value="dark">Dark</MenuItem>
-            </TextField>
-          </Grid>
+            {/* Complexion */}
+            <Grid item xs={12} md={6}>
+              <TextField
+                fullWidth
+                select
+                label="Complexion"
+                id="complexion"
+                name="complexion"
+                defaultValue="">
+                <MenuItem value="fair">Fair</MenuItem>
+                <MenuItem value="medium">Medium</MenuItem>
+                <MenuItem value="dark">Dark</MenuItem>
+              </TextField>
+            </Grid>
 
-          {/* Blood Group */}
-          <Grid item xs={12} md={6} >
-            <TextField
-              fullWidth
-              select
-              label="Blood Group"
-              id="bloodGroup"
-              defaultValue="">
-              <MenuItem value="A+">A+</MenuItem>
-              <MenuItem value="A-">A-</MenuItem>
-              <MenuItem value="B+">B+</MenuItem>
-              <MenuItem value="B-">B-</MenuItem>
-              <MenuItem value="O+">O+</MenuItem>
-              <MenuItem value="O-">O-</MenuItem>
-              <MenuItem value="AB+">AB+</MenuItem>
-              <MenuItem value="AB-">AB-</MenuItem>
-            </TextField>
-          </Grid>
+            {/* Blood Group */}
+            <Grid item xs={12} md={6}>
+              <TextField
+                fullWidth
+                select
+                label="Blood Group"
+                id="bloodGroup"
+                name="bloodGroup"
+                defaultValue="">
+                <MenuItem value="A+">A+</MenuItem>
+                <MenuItem value="A-">A-</MenuItem>
+                <MenuItem value="B+">B+</MenuItem>
+                <MenuItem value="B-">B-</MenuItem>
+                <MenuItem value="O+">O+</MenuItem>
+                <MenuItem value="O-">O-</MenuItem>
+                <MenuItem value="AB+">AB+</MenuItem>
+                <MenuItem value="AB-">AB-</MenuItem>
+              </TextField>
+            </Grid>
 
-          {/* Session */}
-          <Grid item xs={12} md={6}>
-            <TextField
-              fullWidth
-              label="Session"
-              id="session"
-              placeholder="Enter session"
-            />
-          </Grid>
+            {/* Session */}
+            <Grid item xs={12} md={6}>
+              <TextField
+                fullWidth
+                label="Session"
+                id="session"
+                name="session"
+                placeholder="Enter session"
+              />
+            </Grid>
 
-          {/* Department */}
-          <Grid item xs={12} md={6}>
-            <TextField
-              fullWidth
-              label="Department"
-              id="department"
-              placeholder="Enter department"
-            />
-          </Grid>
+            {/* Department */}
+            <Grid item xs={12} md={6}>
+              <TextField
+                fullWidth
+                label="Department"
+                id="department"
+                name="department"
+                placeholder="Enter department"
+              />
+            </Grid>
 
-          {/* Nationality */}
-          <Grid item xs={12} md={6}>
-            <TextField
-              fullWidth
-              label="Nationality"
-              id="nationality"
-              placeholder="Enter nationality"
-            />
+            {/* Nationality */}
+            <Grid item xs={12} md={6}>
+              <TextField
+                fullWidth
+                label="Nationality"
+                id="nationality"
+                name="nationality"
+                placeholder="Enter nationality"
+              />
+            </Grid>
           </Grid>
-        </Grid>
         </form>
       </CardContent>
     </Card>
