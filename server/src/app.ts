1a263fd182cd9af0b470077b4c52c885a193a0fd
shuvojr13 <shuvojr1379@gmail.com>
2025-02-20 15:51:46 +0600

    update app.ts for check api

--- server/src/app.ts
+++ server/src/app.ts
@@ -15,7 +15,6 @@ const limiter = rateLimit({
 });
 
 const corsOption = {
-  origin: ['http://localhost:3000'],
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization'],
   credentials: true,
@@ -42,5 +41,6 @@ app.use('/api', userRoute);
 app.get('/', (req: Request, res: Response) => {
   res.send('my server');
 });
+
 
 export default app;
