2fb3a1f0c285160074a1e345442cae90421df135
suhel45 <suhel.ict45@gmail.com>
2025-02-22 13:04:57 +0600

    familyinfo interface added

---
+++ server/src/interfaces/familyInfo.interface.ts
@@ -0,0 +1,20 @@
+
+export type GardianInfo = {
+    aliveStatus: boolean;
+    profession: string;
+}
+
+export type FamilyInfo = {
+  father: GardianInfo;
+  mother: GardianInfo;
+  siblings: {
+    brotherInfo: {
+      maritalStatus: string;
+    };
+    sisterInfo: {
+      maritalStatus: string;
+    };
+    aboutSiblings: string;
+  };
+  financialStatus: string;
+}
