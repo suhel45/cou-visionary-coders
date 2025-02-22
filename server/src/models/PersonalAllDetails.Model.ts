f38bddebe47d2f88e40982866b9dbe9214e59ff3
suhel45 <suhel.ict45@gmail.com>
2025-02-23 00:49:48 +0600

    personal all detais schema and model creted

---
+++ server/src/models/PersonalAllDetails.Model.ts
@@ -0,0 +1,20 @@
+import { Schema, model } from 'mongoose';
+import { PesonalAllDetails } from '../interfaces/personalAllDetails.interface';
+import { personalInfoSchema } from './personalInfo.Schema';
+import { addressInfoSchema } from './AdressInfo.Schema';
+import { educationInfoSchema } from './EducationalInfo.Schema';
+import { familyInfoSchema } from './FamilyInfo.Schema';
+import { expectedLifePartnerSchema } from './LifePartner.Schema';
+import { contactInfoSchema } from './ContactInfo.Schema';
+
+
+const personalAllDetailsSchema = new Schema<PesonalAllDetails>({
+  personalInfo: { type: personalInfoSchema, required: true },
+  address: { type: addressInfoSchema, required: true },
+  education: { type: educationInfoSchema, required: true },
+  familyInformation: { type: familyInfoSchema, required: true },
+  expectedLifePartner: { type: expectedLifePartnerSchema, required: true },
+  contactInfo: { type: contactInfoSchema, required: true },
+});
+
+export const PersonalAllDetailsModel = model<PesonalAllDetails>('PersonalAllDetails', personalAllDetailsSchema);
