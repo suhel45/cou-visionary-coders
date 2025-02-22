e9baa2a72e125d7282978ae0b29fea94ae1118e2
suhel45 <suhel.ict45@gmail.com>
2025-02-23 00:22:37 +0600

    added contact information schema

---
+++ server/src/models/ContactInfo.Schema.ts
@@ -0,0 +1,8 @@
+import { Schema} from 'mongoose';
+import { ContactInfo } from '../interfaces/contactInfo.interface';
+
+export const contactInfoSchema = new Schema<ContactInfo>({
+  guardiansMobileNumber: { type: String, required: true },
+  candidateMobileNumber: { type: String, required: true },
+  candidateEmail: { type: String, required: true },
+});
