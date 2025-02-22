12a504d0bf1e304fcee4270dca770a11289f0aaf
suhel45 <suhel.ict45@gmail.com>
2025-02-23 00:11:42 +0600

    created expected life partner schema

--- server/src/models/LifePartner.Schema.ts
+++ server/src/models/LifePartner.Schema.ts
@@ -1,25 +1,19 @@
-import { Schema, model } from 'mongoose';
+import { Schema} from 'mongoose';
 import { ExpectedLifePartner } from '../interfaces/lifePartner.interface';
 
-const ageSchema = new Schema({
+const ageHeightSchema = new Schema({
   min: { type: Number, required: true },
   max: { type: Number, required: true },
 });
 
-const heightSchema = new Schema({
-  min: { type: Number, required: true },
-  max: { type: Number, required: true },
-});
 
-const expectedLifePartnerSchema = new Schema<ExpectedLifePartner>({
-  age: { type: ageSchema, required: true },
+export const expectedLifePartnerSchema = new Schema<ExpectedLifePartner>({
+  age: { type: ageHeightSchema, required: true },
   complexion: { type: String, required: true },
-  height: { type: heightSchema, required: true },
+  height: { type: ageHeightSchema, required: true },
   district: { type: String, required: true },
   maritalStatus: { type: String, required: true },
   profession: { type: String, required: true },
   financialCondition: { type: String, required: true },
   expectedQualities: { type: String, required: true },
 });
-
-export const ExpectedLifePartnerModel = model<ExpectedLifePartner>('ExpectedLifePartner', expectedLifePartnerSchema);
