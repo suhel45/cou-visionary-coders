2edf3d0c028d19f3366b1a441f29c3a9cb4af4a9
suhel45 <suhel.ict45@gmail.com>
2025-02-28 12:22:06 +0600

    fix cors origin policy

--- server/src/app.ts
+++ server/src/app.ts
@@ -15,6 +15,7 @@ const limiter = rateLimit({
 });
 
 const corsOption = {
+  origin: ['http://localhost:3000','https://halalbondhon-server.vercel.app/'],
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization'],
   credentials: true,
