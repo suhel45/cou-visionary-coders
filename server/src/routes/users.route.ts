04d47e3eec3facf1abfc2b92e1cfe110ac9490a7
suhel45 <suhel.ict45@gmail.com>
2025-03-10 23:56:14 +0600

    sucessfully get data from DB

--- server/src/routes/users.route.ts
+++ server/src/routes/users.route.ts
@@ -1,7 +1,6 @@
 import { Router } from 'express';
 import { userController } from '../controllers/user.controller';
 import { authController } from '../controllers/auth.controller';
-import { PersonalAllDetailsModel } from './../models/PersonalAllDetails.Model';
 import { verifyToken } from '../middleware/authMiddleware';
 import { personalDetailsController } from '../controllers/personalAllDetails.controller';
 
@@ -13,22 +12,5 @@ router.post('/login/google', authController.loginWithGoogle);
 
 router.post('/profile/biodata', verifyToken, personalDetailsController.Biodata);
 router.get('/profile/biodata/:id', verifyToken, personalDetailsController.GetBiodata);
-
-router.get('/get-data', verifyToken, async (req, res) => {
-  try {
-    const result = await PersonalAllDetailsModel.find({})
-      .populate('users')
-      .lean() // Convert Mongoose document to a plain JSON object
-      .orFail();
-
-    res.json({
-      success: true,
-      data: result,
-    });
-  } catch (error) {
-    console.log(error);
-    res.status(500).json({ success: false, message: 'Failed' });
-  }
-});
 
 export default router;
