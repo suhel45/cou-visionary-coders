fa3134b56c5a4b1a3db52181d44445a2a993b0f4
suhel45 <suhel.ict45@gmail.com>
2025-03-11 13:37:57 +0600

    create fake data to display

--- client/src/components/biodataDetails/FamilyInformation.tsx
+++ client/src/components/biodataDetails/FamilyInformation.tsx
@@ -1,6 +1,18 @@
 import React from 'react'
 
 const FamilyInformation = () => {
+    // Sample fake data 
+  const data = {
+    fatherStatus: 'জীবিত',
+    fatherOccupation: 'শিক্ষক',
+    motherStatus: 'জীবিত',
+    motherOccupation: 'গৃহিণী',
+    brotherInfo: '২ জন ভাই, দুজনই চাকরি করেন',
+    sisterInfo: '১ জন বোন, বিবাহিতা',
+    siblingRelationship: 'খুব ভালো',
+    financialStatus: 'মধ্যবিত্ত'
+  };
+
   return (
     <div>FamilyInformation</div>
   )
