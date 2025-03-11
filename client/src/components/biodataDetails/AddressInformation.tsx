60c044f6829e7f8622d02ae75924cb37dc74471c
suhel45 <suhel.ict45@gmail.com>
2025-03-11 16:19:42 +0600

    define fake data for display

--- client/src/components/biodataDetails/AddressInformation.tsx
+++ client/src/components/biodataDetails/AddressInformation.tsx
@@ -1,6 +1,19 @@
 import React from 'react'
 
 const AddressInformation = () => {
+    const data = {
+        permanentAddress: {
+            "village": "village",
+            "district": "district",
+            "subdistrict": "subdistrict",
+        },
+        presentAddress: {
+            "village": "village",
+            "district": "district",
+            "subdistrict": "subdistrict",
+        },
+      };
+
   return (
     <div>AddressInformation</div>
   )
