87e364aa4d504d4d149932e846f4541aee123652
shuvojr13 <shuvojr1379@gmail.com>
2025-03-13 23:22:16 +0600

    Add: verify profile segment in sidebar of dashboard

--- client/src/components/dashboard/Sidebar.tsx
+++ client/src/components/dashboard/Sidebar.tsx
@@ -1,10 +1,11 @@
 import { useState } from 'react';
-import { LayoutGrid, X, BarChart, User, Settings, Heart, ShoppingCart, HelpCircle } from 'lucide-react';
+import { LayoutGrid, X, BarChart, User, Settings, Heart, ShoppingCart, HelpCircle,ShieldUser } from 'lucide-react';
 import { Link } from 'react-router-dom';
 
 const sidebarLinks = [
   { to: '/dashboard/', label: 'Analytics', icon: BarChart },
   { to: '/dashboard/edit/profile', label: 'Edit Biodata', icon: User },
+  { to: '/dashboard/edit/verify', label: 'Verify Profile', icon: ShieldUser },
   { to: '/dashboard/favourite', label: 'Favourite List', icon: Heart },
   { to: '/dashboard/purchase', label: 'Purchase', icon: ShoppingCart },
   { to: '/dashboard/report', label: 'Support and Report', icon: HelpCircle },
