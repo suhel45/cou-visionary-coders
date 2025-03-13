a9e2a8261be75dde8eb2121367fdb853f75cbe1f
suhel45 <suhel.ict45@gmail.com>
2025-03-14 00:37:29 +0600

    add try catch finally

--- client/src/pages/profile/pages.tsx
+++ client/src/pages/profile/pages.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import UserProfile from '../../components/UserProfile'
 import FamilyInformation from '../../components/profileDetails/FamilyInformation'
 import EducationInformation from '../../components/profileDetails/EducationalInformation'
@@ -8,7 +8,17 @@ const pages = () => {
   const [userData, setUserData] = useState(null)
   const [loading, setLoading] = useState(true);
 
-  
+  useEffect(()) => {
+    const fetchData = async () => {
+      try {
+        
+      }catch (error){
+
+      }finally{
+        setLoading(false)
+      }
+    }
+  }
   return (
     <div className="flex flex-col md:flex-row  gap-2  bg-gray-50 h-auto ">
         <div className="flex flex-col items-center md:items-stretch bg-gray-50 md:w-1/3">
