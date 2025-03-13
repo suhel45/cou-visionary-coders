52d49ec8e9108b947686ebf20cbdbc27ce6e41bd
suhel45 <suhel.ict45@gmail.com>
2025-03-14 00:34:27 +0600

    add usestate of userData and loading

--- client/src/pages/profile/pages.tsx
+++ client/src/pages/profile/pages.tsx
@@ -1,9 +1,14 @@
-import React from 'react'
+import React, { useState } from 'react'
 import UserProfile from '../../components/UserProfile'
 import FamilyInformation from '../../components/profileDetails/FamilyInformation'
 import EducationInformation from '../../components/profileDetails/EducationalInformation'
 import AddressInformation from '../../components/profileDetails/AddressInformation'
+
 const pages = () => {
+  const [userData, setUserData] = useState(null)
+  const [loading, setLoading] = useState(true);
+
+  
   return (
     <div className="flex flex-col md:flex-row  gap-2  bg-gray-50 h-auto ">
         <div className="flex flex-col items-center md:items-stretch bg-gray-50 md:w-1/3">
