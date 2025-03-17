238b39331bd0444a4bc3bba9498876665a94b1c9
shuvojr13 <shuvojr1379@gmail.com>
2025-03-17 21:59:06 +0600

    feat: create and add analytical component on dashboard

---
+++ client/src/components/dashboard/analytics/Analytic.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import ProfileView from './ProfileView'
+const Analytic = () => {
+  return (
+    <div>
+        <ProfileView/>
+    </div>
+  )
+}
+
+export default Analytic
