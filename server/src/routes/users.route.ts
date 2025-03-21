d7c821559609db6e1339980c66c1c91bc7fc6a72
suhel45 <suhel.ict45@gmail.com>
2025-03-22 02:31:44 +0600

    add public api for biodataList

--- server/src/routes/users.route.ts
+++ server/src/routes/users.route.ts
@@ -3,7 +3,6 @@ import { userController } from '../controllers/user.controller';
 import { authController } from '../controllers/auth.controller';
 import { verifyToken } from '../middleware/authMiddleware';
 import { personalDetailsController } from '../controllers/personalAllDetails.controller';
-import { PersonalAllDetailsModel } from '../models/PersonalAllDetails.Model';
 
 const router = Router();
 
@@ -12,28 +11,11 @@ router.post('/login', userController.loginUser);
 router.post('/login/google', authController.loginWithGoogle);
 
 router.post('/profile/biodata', verifyToken, personalDetailsController.Biodata);
-
-router.get('/get-data', verifyToken, async (req, res) => {
-  try {
-    const result = await PersonalAllDetailsModel.find({})
-      .populate('users')
-      .lean() // Convert Mongoose document to a plain JSON object
-      .orFail();
-
-    res.json({
-      success: true,
-      data: result,
-    });
-  } catch (error) {
-    console.log(error);
-    res.status(500).json({ success: false, message: 'Failed' });
-  }
-});
-router.post('/profile/biodata', verifyToken, personalDetailsController.Biodata);
 router.get(
   '/profile/biodata',
   verifyToken,
   personalDetailsController.GetBiodata,
 );
+routet.get('/biodata')
 
 export default router;
