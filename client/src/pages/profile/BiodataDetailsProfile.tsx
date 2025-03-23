4d35a27a4e4b03e557b73021ce71f9079c71d359
suhel45 <suhel.ict45@gmail.com>
2025-03-24 03:31:51 +0600

    added loader and error

--- client/src/pages/profile/BiodataDetailsProfile.tsx
+++ client/src/pages/profile/BiodataDetailsProfile.tsx
@@ -1,6 +1,14 @@
 import { useQuery } from '@tanstack/react-query';
+import axios from 'axios';
 import React from 'react'
 import { useParams } from 'react-router-dom'
+import Loading from '../../utils/Loading/Loading';
+
+const fetchBiodataDetail = async (id: string) => {
+    const response = await axios.get(`http://localhost:3000/api/profile/biodata/${id}`);
+    console.log(response.data);
+    return response.data;
+  };
 
 const BiodataDetailsProfile = () => {
     const {id} = useParams<{id: string}>()
@@ -11,7 +19,18 @@ const BiodataDetailsProfile = () => {
         enabled: !!id,
       });
 
-      
+      if (isLoading) {
+        return <Loading />;
+      }
+
+      if (error) {
+        return (
+          <div className="flex justify-center items-center min-h-screen text-red-600">
+            {(error as Error).message}
+          </div>
+        );
+      }
+
   return (
     <div>BiodataDetailsProfile</div>
   )
