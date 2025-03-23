8ac23d8f9921abef2c7ff6e15e7e6519bef8da6f
suhel45 <suhel.ict45@gmail.com>
2025-03-24 02:40:29 +0600

    extract id from useparams

--- client/src/pages/profile/BiodataDetailsProfile.tsx
+++ client/src/pages/profile/BiodataDetailsProfile.tsx
@@ -1,6 +1,8 @@
 import React from 'react'
+import { useParams } from 'react-router-dom'
 
 const BiodataDetailsProfile = () => {
+    const {id} = useParams<{id: string}>()
   return (
     <div>BiodataDetailsProfile</div>
   )
