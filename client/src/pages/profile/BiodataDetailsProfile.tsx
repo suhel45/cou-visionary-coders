aa0d8d738bf60b808063c93db49ca5a22c8ba462
suhel45 <suhel.ict45@gmail.com>
2025-03-24 02:44:58 +0600

    add useQuery to fetch bio data details

--- client/src/pages/profile/BiodataDetailsProfile.tsx
+++ client/src/pages/profile/BiodataDetailsProfile.tsx
@@ -1,8 +1,17 @@
+import { useQuery } from '@tanstack/react-query';
 import React from 'react'
 import { useParams } from 'react-router-dom'
 
 const BiodataDetailsProfile = () => {
     const {id} = useParams<{id: string}>()
+
+    const { data, isLoading, error } = useQuery({
+        queryKey: ['BiodataDetailsProfile', id],
+        queryFn: () => fetchBiodataDetail(id as string),
+        enabled: !!id,
+      });
+
+      
   return (
     <div>BiodataDetailsProfile</div>
   )
