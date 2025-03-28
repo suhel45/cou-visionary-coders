a0ad5627ef03123700e22b5cadb0bec759752e4c
suhel45 <suhel.ict45@gmail.com>
2025-03-29 03:37:03 +0600

    added google button

--- client/src/components/GoogleSignIn.jsx
+++ client/src/components/GoogleSignIn.jsx
@@ -1,9 +1,17 @@
-import React from 'react'
-
 const GoogleSignIn = () => {
   return (
-    <div>GoogleSignIn</div>
-  )
-}
+    <button
+    onClick={signInWithGoogle}
+      className="cursor-pointer flex items-center justify-center w-full bg-white border border-gray-300 text-gray-700 p-2 rounded-md shadow-md hover:bg-gray-100"
+    >
+      <img
+        src="https://imgs.search.brave.com/0dfkmCFWC2zrjWCenB_rDnfa_wKBmKDmxG4qSB78iQs/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly9zdGF0/aWMtMDAuaWNvbmR1/Y2suY29tL2Fzc2V0/cy4wMC9nb29nbGUt/aWNvbi01MTJ4NTEy/LXRxYzllbDNyLnBu/Zw"
+        alt="Google"
+        className="w-5 h-5 mr-2"
+      />{' '}
+      Sign in with Google
+    </button>
+  );
+};
 
-export default GoogleSignIn
+export default GoogleSignIn;
--- client/src/components/Login.tsx
+++ client/src/components/Login.tsx
@@ -5,6 +5,7 @@ import { useForm, SubmitHandler } from 'react-hook-form';
 import { AuthContext } from '../Hooks/contextApi/UserContext';
 import { IFormInput } from '../interfaces/Login.interface';
 import { Eye, EyeOff } from 'lucide-react';
+import GoogleSignIn from './GoogleSignIn';
 
 const Login: React.FC = () => {
   const [loading, setLoading] = useState(false);
@@ -156,14 +157,7 @@ const Login: React.FC = () => {
         </div>
 
         {/* Google Sign-In */}
-        <button className="cursor-pointer flex items-center justify-center w-full bg-white border border-gray-300 text-gray-700 p-2 rounded-md shadow-md hover:bg-gray-100">
-          <img
-            src="https://imgs.search.brave.com/0dfkmCFWC2zrjWCenB_rDnfa_wKBmKDmxG4qSB78iQs/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly9zdGF0/aWMtMDAuaWNvbmR1/Y2suY29tL2Fzc2V0/cy4wMC9nb29nbGUt/aWNvbi01MTJ4NTEy/LXRxYzllbDNyLnBu/Zw"
-            alt="Google"
-            className="w-5 h-5 mr-2"
-          />{' '}
-          Sign in with Google
-        </button>
+        <GoogleSignIn />
 
         <p className="mt-6 text-center text-sm md:text-lg text-gray-500">
           Not a member?
