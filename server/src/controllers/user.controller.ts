8fc966a73a3ad6aacd4b0f1856920d6c8046792d
suhel45 <suhel.ict45@gmail.com>
2025-04-03 20:16:37 +0600

    replace console to logger

--- server/src/controllers/user.controller.ts
+++ server/src/controllers/user.controller.ts
@@ -2,25 +2,27 @@ import { Request, Response } from 'express';
 import dotenv from 'dotenv';
 import { userService } from '../services/users.service';
 import { ILoginInfo, IUser } from '../interfaces/users.interface';
+import logger from '../utils/logger.util';
 
 dotenv.config();
 
 const createUser = async (req: Request, res: Response) => {
   try {
     const userInfo: IUser = req.body;
-    //will call service function to send this data
     const result = await userService.createUserIntoDB(userInfo);
-    //send response
+    
     res.status(200).json({
       success: true,
       message: 'User registered successfully',
       data: result,
     });
+
   } catch (error) {
     if (error instanceof Error && error.message === 'User already exists!') {
+      logger.error(`User already exists: ${error.message}`);
       res.status(400).json({ message: 'User already exists!' });
     } else {
-      console.error('Unexpected error:', error);
+      logger.error(`Unexpected error of createuser:${error}`);
       res.status(500).json({
         message: 'An unexpected error occurred. Please try again later.',
       });
@@ -49,7 +51,9 @@ const loginUser = async (req: Request, res: Response) => {
       userId: userId,
     });
   } catch (error) {
-    console.error('Unexpected error:', error);
+    if(error instanceof Error){
+      logger.error(`Unexpected error:${error.message}`);
+    }
     res.status(500).json({
       message: 'An unexpected error occurred. Please try again later.',
     });
@@ -70,7 +74,9 @@ const resetPassword = async (req: Request, res: Response) => {
       message: result,
     });
   } catch (error) {
-    console.error('Reset Password Error:', error);
+    if(error instanceof Error){
+        logger.error(`Reset Password Error:${error.message}`);
+    }
     res.status(500).json({
       message: 'Server error occurred',
     });
@@ -97,7 +103,9 @@ const forgotPassword = async (req: Request, res: Response) => {
     }
 
   } catch (error) {
-    console.error('Forget Password Error:', error);
+    if (error instanceof Error) {
+      logger.error(`Forget Password Error:${error.message}`);
+    }
     res.status(500).json({
       success: false,
       message: 'Server error occurred.please try again later',
@@ -113,8 +121,11 @@ const resetPasswordWithToken = async (req: Request, res: Response) => {
     res.status(200).json({
       message: result,
     });
+
   } catch (error) {
-    console.error('Reset Password with Token Error:', error);
+    if (error instanceof Error) {
+      logger.error(`Reset Password with Token Error:${error.message}`);
+    }
     res.status(500).json({
       message: 'Server error occurred',
     });
