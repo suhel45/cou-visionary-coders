d7923cb2cdaf3faad1bc3eb72e35af75839f0c5a
suhel45 <suhel.ict45@gmail.com>
2025-04-12 21:29:38 +0600

    add delete biodata controller function

---
+++ server/src/controllers/deleteBiodata.controller.ts
@@ -0,0 +1,26 @@
+import { Request, Response } from "express";
+
+interface CustomRequest extends Request {
+  user: {
+    id: string;
+  };
+}
+
+export const deleteBiodataController = async (req:Request, res:Response) => {
+  try {
+    const biodataId = (req as CustomRequest).user.id;
+
+    const deletedBiodata = await deleteBiodata(biodataId);
+
+    res.status(200).json({
+      message: 'Biodata deleted successfully',
+      data: deletedBiodata,
+    });
+  } catch (error) {
+    // Handle errors and send an error response
+    res.status(500).json({
+      message: 'Error deleting biodata',
+      error: error.message,
+    });
+  }
+}
