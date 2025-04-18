1d4a17d3deeefe5645fe122e9cb84ae091b23d1b
suhel45 <suhel.ict45@gmail.com>
2025-04-18 17:53:25 +0600

    server deploy sucessfully

--- server/src/app.ts
+++ server/src/app.ts
@@ -12,11 +12,17 @@ dotenv.config();
 
 const app = express();
 
+app.set('trust proxy', 1);
+
 const limiter = rateLimit({
   windowMs: 15 * 60 * 1000,
   limit: 100,
   standardHeaders: 'draft-8',
   legacyHeaders: false,
+  validate: {
+    trustProxy: false,
+    xForwardedForHeader: false,
+  },
 });
 
 const corsOption = {
