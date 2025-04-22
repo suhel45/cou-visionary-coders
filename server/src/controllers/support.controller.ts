a2e5512c8226d9854c8d18018705afec55dc48bd
suhel45 <suhel.ict45@gmail.com>
2025-04-22 12:45:17 +0600

    add support controller function

---
+++ server/src/controllers/support.controller.ts
@@ -0,0 +1,12 @@
+import { Request, Response } from "express";
+
+const addToSupport = async (req:Request, res:Response) => {
+  try {
+    const { message } = req.body;
+
+    res.status(200).json({ message: 'Support request submitted successfully' });
+  } catch (error) {
+    console.error('Error adding to support:', error);
+    return res.status(500).json({ error: 'Internal server error' });
+  }
+}
