50e0043455e65707517163bad9c29cb6cde08e09
suhel45 <suhel.ict45@gmail.com>
2025-04-22 12:46:43 +0600

    extract user id

--- server/src/controllers/support.controller.ts
+++ server/src/controllers/support.controller.ts
@@ -1,7 +1,14 @@
 import { Request, Response } from "express";
 
+interface CustomRequest extends Request {
+    user: {
+      id: string;
+    };
+  }
+  
 const addToSupport = async (req:Request, res:Response) => {
   try {
+    const userId = (req as CustomRequest).user.id;
     const { message } = req.body;
 
     res.status(200).json({ message: 'Support request submitted successfully' });
