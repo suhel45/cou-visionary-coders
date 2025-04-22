a8a98217d78456a754ba4d6a06e148c81f65912a
suhel45 <suhel.ict45@gmail.com>
2025-04-22 11:22:06 +0600

    save report to db

--- server/src/services/report.service.ts
+++ server/src/services/report.service.ts
@@ -1,11 +1,23 @@
-import { IReport } from "../interfaces/report.interface";
 import { PersonalAllDetailsModel } from "../models/PersonalAllDetails.Model";
+import { ReportModel } from "../models/report.model";
 
 const createReport = async (userId:string, biodataNo:number, reason:string, reasonDetails:string) => {
-    
+
     // Verify biodata exists
     const biodata = await PersonalAllDetailsModel.findOne({ biodataNo });
     if (!biodata) {
         throw new Error("Biodata not found!");
     }
+
+    // Create the report
+    const newReport = new ReportModel({
+        biodataNo,
+        reason,
+        reasonDetails,
+        reporter: userId
+      });
+      
+     const report = await newReport.save();
+     return report;
+
 }
