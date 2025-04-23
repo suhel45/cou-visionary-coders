cf04279af76c2452bbc02a012e0d1e203fe2d4dd
suhel45 <suhel.ict45@gmail.com>
2025-04-23 19:39:50 +0600

    cookie option change

--- server/src/controllers/user.controller.ts
+++ server/src/controllers/user.controller.ts
@@ -38,8 +38,8 @@ const loginUser = async (req: Request, res: Response) => {
     //set the token as an httpOnly cookie
     res.cookie('token', token, {
       httpOnly: true,
-      secure: false,
-      sameSite: 'lax',
+      secure: true,
+      sameSite: 'strict',
       path: '/',
       maxAge: 3600000, // 1 hour
     });
