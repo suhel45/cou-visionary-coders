761114661a6f903faa7eb68e0214f7eac758aa05
shuvojr13 <shuvojr1379@gmail.com>
2025-04-25 16:10:07 +0600

    ui design fix

--- client/src/components/Count.tsx
+++ client/src/components/Count.tsx
@@ -11,7 +11,7 @@ const Count: React.FC<CountProps> = ({ imageSrc, title, cnt }) => {
     <div className="flex flex-col items-center justify-center border border-gray-300 p-6 basis-full rounded-lg shadow-sm hover:shadow-lg bg-gray-50">
       <img src={imageSrc} alt={title} className="w-auto h-24 mb-4" />
       <p className="font-bold text-xl text-blue-900 py-2">{title}</p>
-      <h4 className="text-center bg-gray-200 rounded-lg font-bold text-violet-900 text-xl p-4">
+      <h4 className="text-center bg-gray-200 rounded-lg font-bold text-violet-900 text-xl py-4 px-8">
         {cnt}
       </h4>
     </div>
