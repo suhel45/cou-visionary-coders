9956bc96c38f2a4df6a13dbf5a9de586b5344e15
shuvojr13 <shuvojr1379@gmail.com>
2025-04-26 11:32:45 +0600

    add admin overviews

--- server/src/routes/admin.route.ts
+++ server/src/routes/admin.route.ts
@@ -3,7 +3,7 @@ import { isAdmin } from '../middleware/admin.middleware';
 import { reportController } from '../controllers/report.controller';
 import { verifyToken } from '../middleware/authMiddleware';
 import { supportController } from '../controllers/support.controller';
-import { getAllPendingVerifications,approveVerification } from '../controllers/verify.controller';
+import { getAllPendingVerifications,approveVerification,rejectVerification } from '../controllers/verify.controller';
 const router = express.Router();
 
 router.get(
@@ -20,5 +20,6 @@ router.get(
 );
 router.get('/all-status',getAllPendingVerifications);
 router.post('/verify-approve/:userId',approveVerification);
+router.post('/verify-reject/:userId',rejectVerification);
 
 export const adminRoutes = router;
